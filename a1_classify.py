b6c8ea4a4c523ed6f531c6bbc710a23fb575ccb4
Jiatao Xiang <a389157511@gmail.com>
2021-01-18 00:39:53 -0500

    first commit

---
+++ a1_classify.py
@@ -0,0 +1,276 @@
+  #  * This code is provided solely for the personal and private use of students
+#  * taking the CSC401 course at the University of Toronto. Copying for purposes
+#  * other than this use is expressly prohibited. All forms of distribution of
+#  * this code, including but not limited to public repositories on GitHub,
+#  * GitLab, Bitbucket, or any other online platform, whether as given or with
+#  * any changes, are expressly prohibited.
+#  
+#  * All of the files in this directory and all subdirectories are:
+#  * Copyright (c) 2020 Frank Rudzicz
+
+import argparse
+import os
+from scipy.stats import ttest_rel
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import SGDClassifier  
+
+# set the random state for reproducibility 
+import numpy as np
+np.random.seed(401)
+
+# global variables
+classifiers = ["SGDClassifier", "GaussianNB", "RandomForestClassifier", "MLPClassifier", "AdaBoostClassifier"]
+parameters = ["()", "()", "(max_depth=5, n_estimators=10)","(alpha=0.05)", "()"]
+
+def accuracy(C):
+    ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
+    # print ('TODO')
+    return C.trace() / C.sum()
+
+
+
+def recall(C):
+    ''' Compute recall given Numpy array confusion matrix C. Returns a list of floating point values '''
+    # print ('TODO')
+    return C.diagonal()/C.sum(axis=1)
+
+
+def precision(C):
+    ''' Compute precision given Numpy array confusion matrix C. Returns a list of floating point values '''
+    # print ('TODO')
+    return C.diagonal()/C.sum(axis=0)
+    
+
+def class31(output_dir, X_train, X_test, y_train, y_test):
+    ''' This function performs experiment 3.1
+    
+    Parameters
+       output_dir: path of directory to write output to
+       X_train: NumPy array, with the selected training features
+       X_test: NumPy array, with the selected testing features
+       y_train: NumPy array, with the selected training classes
+       y_test: NumPy array, with the selected testing classes
+
+    Returns:      
+       i: int, the index of the supposed best classifier
+    '''
+    # print('TODO Section 3.1')
+    
+    iBest = None
+    max_accuracy = -float("inf")
+    with open(f"{output_dir}/a1_3.1.txt", "w") as outf:
+        # For each classifier, compute results and write the following output:
+        for i in range(len(classifiers)):
+            classifier = eval(classifiers[i] + parameters[i])
+            classifier_name = classifier.__class__.__name__
+            # train classifier
+            classifier.fit(X_train, y_train)
+
+            y_predictions = classifier.predict(X_test)
+            conf_matrix = confusion_matrix(y_test, y_predictions)
+            accuracy = accuracy(conf_matrix)
+            if accuracy > max_accuracy:
+                iBest = i
+                max_accuracy = accuracy
+            recall = recall(conf_matrix)
+            precision = precision(conf_matrix)
+
+            outf.write(f'Results for {classifier_name}:\n')  # Classifier name
+            outf.write(f'\tAccuracy: {accuracy:.4f}\n')
+            outf.write(f'\tRecall: {[round(item, 4) for item in recall]}\n')
+            outf.write(f'\tPrecision: {[round(item, 4) for item in precision]}\n')
+            outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
+        # pass
+
+    return iBest
+
+
+def class32(output_dir, X_train, X_test, y_train, y_test, iBest):
+    ''' This function performs experiment 3.2
+    
+    Parameters:
+       output_dir: path of directory to write output to
+       X_train: NumPy array, with the selected training features
+       X_test: NumPy array, with the selected testing features
+       y_train: NumPy array, with the selected training classes
+       y_test: NumPy array, with the selected testing classes
+       iBest: int, the index of the supposed best classifier (from task 3.1)  
+
+    Returns:
+       X_1k: numPy array, just 1K rows of X_train
+       y_1k: numPy array, just 1K rows of y_train
+   '''
+    # print('TODO Section 3.2')
+    num_samples = [1000, 5000, 10000, 15000, 20000]
+    with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
+        # For each number of training examples, compute results and write
+        # the following output:
+        #     outf.write(f'{num_train}: {accuracy:.4f}\n'))
+
+        # pass
+        X_1k, y_1k = None, None
+        for num_train in num_samples:
+            selected_indices = np.random.choice(X_train.shape[0], num_train, replace=False)
+            x_training = X_train[selected_indices]
+            y_training = y_train[selected_indices]
+            classifier = eval(classifiers[iBest] + parameters[iBest])
+
+            classifier.fit(x_training, y_training)
+            y_predictions = classifier.predict(X_test)
+            conf_maxtrix = confusion_matrix(y_test, y_predictions)
+
+            accuracy = accuracy(conf_maxtrix)
+            outf.write(f'{num_train}: {accuracy:.4f}\n')
+
+            if num_train == 1000:
+                X_1k, y_1k = x_training, y_training
+
+    return (X_1k, y_1k)
+
+# helper for class33
+def get_top_5_features_accuracy(X_train, y_train, X_test, y_test, i):
+    classifier = eval(classifiers[i] + parameters[i])
+    selector = SelectKBest(f_classif, k=5)
+    X_new = selector.fit_transform(X_train, y_train)
+
+    feature_indices = selector.get_support(indices=True)
+
+    x_new_test = np.take(X_test, feature_indices, axis=1)
+    classifier.fit(X_new, y_train)
+    y_predictions = classifier.predict(x_new_test)
+    conf_matrix = confusion_matrix(y_test, y_predictions)
+
+    acc = accuracy(conf_matrix)
+    return acc, feature_indices
+
+
+def class33(output_dir, X_train, X_test, y_train, y_test, i, X_1k, y_1k):
+    ''' This function performs experiment 3.3
+    
+    Parameters:
+       output_dir: path of directory to write output to
+       X_train: NumPy array, with the selected training features
+       X_test: NumPy array, with the selected testing features
+       y_train: NumPy array, with the selected training classes
+       y_test: NumPy array, with the selected testing classes
+       i: int, the index of the supposed best classifier (from task 3.1)  
+       X_1k: numPy array, just 1K rows of X_train (from task 3.2)
+       y_1k: numPy array, just 1K rows of y_train (from task 3.2)
+    '''
+    # print('TODO Section 3.3')
+    
+    with open(f"{output_dir}/a1_3.3.txt", "w") as outf:
+        # Prepare the variables with corresponding names, then uncomment
+        # this, so it writes them to outf.
+        # for each number of features k_feat, write the p-values for
+        # that number of features:
+        for k_feat in [5, 50]:
+            selector = SelectKBest(f_classif, k=k_feat)
+            # X_new = selector.fit_transform(X_train, y_train)
+            p_values = selector.pvalues_
+            outf.write(f'{k_feat} p-values: {[round(pval, 4) for pval in p_values]}\n')
+
+        accuracy_1k, top_features_1k = get_top_5_features_accuracy(X_1k, y_1k, X_test, y_test, i)
+        accuracy_full, top_features_full = get_top_5_features_accuracy(X_train, y_train, X_test, y_test, i)
+
+        feature_intersection = np.intersect1d(top_features_1k, top_features_full)
+        top_5 = top_features_full
+
+        outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
+        outf.write(f'Accuracy for full dataset: {accuracy_full:.4f}\n')
+        outf.write(f'Chosen feature intersection: {feature_intersection}\n')
+        outf.write(f'Top-5 at higher: {top_5}\n')
+        
+
+
+def class34(output_dir, X_train, X_test, y_train, y_test, i):
+    ''' This function performs experiment 3.4
+    
+    Parameters
+       output_dir: path of directory to write output to
+       X_train: NumPy array, with the selected training features
+       X_test: NumPy array, with the selected testing features
+       y_train: NumPy array, with the selected training classes
+       y_test: NumPy array, with the selected testing classes
+       i: int, the index of the supposed best classifier (from task 3.1)  
+        '''
+    # print('TODO Section 3.4')
+    
+    X = np.concatenate(X_train, X_test)
+    Y = np.concatenate(y_train, y_test)
+    with open(f"{output_dir}/a1_3.4.txt", "w") as outf:
+        # Prepare kfold_accuracies, then uncomment this, so it writes them to outf.
+        # for each fold:
+        #     outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in kfold_accuracies]}\n')
+        # outf.write(f'p-values: {[round(pval, 4) for pval in p_values]}\n')
+        # pass
+        Kf = KFold(shuffle=True)
+        classifier_accuracies = []
+        for train_index, test_index in Kf.split(X):
+            x_train, y_train = X[train_index], Y[train_index]
+            x_test, y_test = X[test_index], Y[test_index]
+
+            kfold_accuracies = np.zeros(5)
+
+            for j in range(len(classifiers)):
+                classifier = eval(classifiers[i] + parameters[i])
+                classifier.fit(x_train, y_train)
+                y_predictions = classifier.predict(x_test)
+                conf_matrix = confusion_matrix(y_test, y_predictions)
+
+                accuracy = accuracy(conf_matrix)
+                kfold_accuracies[i] = accuracy
+            classifier_accuracies.append(kfold_accuracies)
+            outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in kfold_accuracies]}\n')
+
+        p_values = []
+        classifier_accuracies = np.array(classifier_accuracies)
+        classifier_accuracies = classifier_accuracies.transpose()
+        for j in range(len(classifiers)):
+            if j == i:
+                continue
+            S= ttest_rel(classifier_accuracies[j], classifier_accuracies[i])
+            p_values.append(S.pvalue)
+        outf.write(f'p-values: {[round(pval, 4) for pval in p_values]}\n')
+
+
+            
+        
+            
+
+
+
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="the input npz file from Task 2", required=True)
+    parser.add_argument(
+        "-o", "--output_dir",
+        help="The directory to write a1_3.X.txt files to.",
+        default=os.path.dirname(os.path.dirname(__file__)))
+    args = parser.parse_args()
+    
+    # TODO: load data and split into train and test.
+    data = np.load(args.input) # Need to be tested
+    x = data[:, :173]
+    y = data[:, 173]
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
+    # TODO : complete each classification experiment, in sequence.
+
+    output_dir = args.output_dir
+    
+    iBest = class31(output_dir, X_train, X_test, y_train, y_test)
+
+    X_1k, y_1k = class32(output_dir, X_train, X_test, y_train, y_test, iBest)
+
+    class33(output_dir, X_train, X_test, y_train, y_test, iBest, X_1k, y_1k)
+
+    class34(output_dir, X_train, X_test, y_train, y_test, iBest)
---
+++ a1_extractFeatures.py
@@ -0,0 +1,233 @@
+#  * This code is provided solely for the personal and private use of students
+#  * taking the CSC401 course at the University of Toronto. Copying for purposes
+#  * other than this use is expressly prohibited. All forms of distribution of
+#  * this code, including but not limited to public repositories on GitHub,
+#  * GitLab, Bitbucket, or any other online platform, whether as given or with
+#  * any changes, are expressly prohibited.
+#  
+#  * All of the files in this directory and all subdirectories are:
+#  * Copyright (c) 2020 Frank Rudzicz
+
+import numpy as np
+import argparse
+import json
+
+# self import lib
+import string
+import csv
+import numpy as np
+import os
+
+# Provided wordlists.
+FIRST_PERSON_PRONOUNS = {
+    'i', 'me', 'my', 'mine', 'we', 'us', 'our', 'ours'}
+SECOND_PERSON_PRONOUNS = {
+    'you', 'your', 'yours', 'u', 'ur', 'urs'}
+THIRD_PERSON_PRONOUNS = {
+    'he', 'him', 'his', 'she', 'her', 'hers', 'it', 'its', 'they', 'them',
+    'their', 'theirs'}
+SLANG = {
+    'smh', 'fwb', 'lmfao', 'lmao', 'lms', 'tbh', 'rofl', 'wtf', 'bff',
+    'wyd', 'lylc', 'brb', 'atm', 'imao', 'sml', 'btw', 'bw', 'imho', 'fyi',
+    'ppl', 'sob', 'ttyl', 'imo', 'ltr', 'thx', 'kk', 'omg', 'omfg', 'ttys',
+    'afn', 'bbs', 'cya', 'ez', 'f2f', 'gtr', 'ic', 'jk', 'k', 'ly', 'ya',
+    'nm', 'np', 'plz', 'ru', 'so', 'tc', 'tmi', 'ym', 'ur', 'u', 'sol', 'fml'}
+
+# get global variable for norms
+BGNorm = {}
+with open("/u/cs401/Wordlists/BristolNorms+GilhoolyLogie.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+    for row in csv_reader:
+        BGNorm[row["Word"]] = [row['AoA (100-700)'], row['IMG'], row['FAM']]
+
+Warringer = {}
+with open("/u/cs401/Wordlists/Ratings_Warriner_et_al.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+    for row in csv_reader:
+        BGNorm[row["Word"]] = [row['V.Mean.Sum'], row['A.Mean.Sum'], row['D.Mean.Sum']]
+
+classes = ["Left", "Center", "Right", "Alt"]
+LIWC = []
+ids = [{}, {}, {}, {}]
+for i, subroute in enumerate(classes):
+    id_file_name = os.path.join("/u/cs401/A1/feats/", subroute + "_IDs.txt")
+    with open(id_file_name, mode="r") as id_file:
+        for order, line in enumerate(id_file):  
+            LIWC[i][line.rstrip("\n")] = order
+    feats_file_name = os.path.join("/u/cs401/A1/feats/", subroute + "_feats.dat.npy")
+    LIWC.append(np.load(feats_file_name))
+
+
+# helper
+def is_multi_punctation(word):
+    return len(word) > 1 and all([char in string.punctuation for char in word])
+
+def extract1(comment):
+    ''' This function extracts features from a single comment
+
+    Parameters:
+        comment : string, the body of a comment (after preprocessing)
+
+    Returns:
+        feats : numpy Array, a 173-length vector of floating point features (only the first 29 are expected to be filled, here)
+    '''   
+    # print('TODO')
+    feats = np.zeros(174)
+    sentences = comment.split("\n")
+    if sentences[-1] == "\n": # if it ends with \n, remove empty string
+        sentences.pop() 
+    # TODO: Extract features that rely on capitalization.
+    total_num_sentences = len(sentences)
+    total_num_tokens = 0
+    AoA_list, IMG_list, FAM_list = [], [], []
+    V_list, A_list, D_list = [], [], []
+    for sent in sentences:
+        tokens = sent.split(" ")
+        total_num_tokens += len(tokens)
+        for i, token in enumerate(tokens):
+            # if token.find("/") == -1: # empty string or a space
+            #     continue
+            word, tag = token.split("/")
+            # feature 1: Number of tokens in uppercase (≥ 3 letters long)
+            if word.isupper() and len(word) >= 3: feats[0] += 1
+    # TODO: Lowercase the text in comment. Be careful not to lowercase the tags. (e.g. "Dog/NN" -> "dog/NN").
+            word = word.lower()
+    # TODO: Extract features that do not rely on capitalization.
+            # feature 2: Number of first-person pronouns
+            if word in FIRST_PERSON_PRONOUNS: feats[1] += 1
+            # feature 3: Number of second-person pronouns
+            if word in SECOND_PERSON_PRONOUNS: feats[2] += 1
+            # feature 4: Number of third-person pronouns
+            if word in THIRD_PERSON_PRONOUNS: feats[3] += 1
+            # feature 5: Number of coordinating conjunctions
+            if tag == "CC": feats[4] += 1
+            # feature 6: Number of past-tense verbs
+            if tag == "VBD": feats[5] += 1
+            # feature 7: Number of future-tense verbs
+            if word.endswith("'ll") or word in {"will", "gonna"}: feats[6] += 1
+            if tag == "VB" and i > 1 and tokens[i - 1].startswith("to/") and tokens[i - 2].startswith("going/"): feats[6] += 1
+            # feature 8: Number of commas
+            if word == ",": feats[7] += 1
+            # feature 9: Number of multi-character punctuation tokens
+            if is_multi_punctation(word): feats[8] += 1
+            # feature 10: Number of common nouns
+            if tag == "NN" or tag == "NNS": feats[9] += 1
+            # feature 11: Number of proper nouns
+            if tag == "NNP" or tag == "NNPS": feats[10] += 1
+            # feature 12: Number of adverbs
+            if tag in {"RB", "RBR", "RBS"}: feats[11] += 1
+            # feature 13: Number of wh- words
+            if tag in {"WDT", "WP", "WP$", "WRB"}: feats[12] += 1
+            # feature 14: Number of slang acronyms
+            if word in SLANG: feats[13] += 1  
+            # feature 16: Average length of tokens, excluding punctuation-only tokens, in characters
+            if is_multi_punctation(word) or (len(word) == 0 and word in string.punctuation): feats[15] += 1
+            # feature 18: Average of AoA (100-700) from Bristol, Gilhooly, and Logie norms
+            # feature 19: Average of IMG from Bristol, Gilhooly, and Logie norms
+            # feature 20: Average of FAM from Bristol, Gilhooly, and Logie norms
+            # feature 21: Standard deviation of AoA (100-700) from Bristol, Gilhooly, and Logie norms
+            # feature 22: Standard deviation of IMG from Bristol, Gilhooly, and Logie norms
+            # feature 23: Standard deviation of FAM from Bristol, Gilhooly, and Logie norms
+            if word in BGNorm:
+                AoA, IMG, FAM = BGNorm[word]
+                AoA_list.append(AoA)
+                IMG_list.append(IMG)
+                FAM_list.append(FAM)
+            # feature 24: Average of V.Mean.Sum from Warringer norms
+            # feature 25: Average of A.Mean.Sum from Warringer norms
+            # feature 26: Average of D.Mean.Sum from Warringer norms
+            # feature 27: Standard deviation of V.Mean.Sum from Warringer norms
+            # feature 28: Standard deviation of A.Mean.Sum from Warringer norms
+            # feature 29: Standard deviation of D.Mean.Sum from Warringer norms
+            if word in Warringer:
+                V, A, D = Warringer[word]
+                V_list.append(V)
+                A_list.append(A)
+                D_list.append(D)
+    # feature 15: Average length of sentences, in tokens
+    feats[14] =  total_num_tokens/total_num_sentences
+    # feature 16 updates
+    feats[15] /= total_num_tokens
+    # feature 17: Number of sentences.
+    feats[16] = total_num_sentences
+    # convert to numpy array
+    AoA_list = np.array(AoA_list)
+    IMG_list = np.array(IMG_list)
+    FAM_list = np.array(FAM_list)
+    V_list = np.array(V_list)
+    A_list = np.array(A_list)
+    D_list = np.array(D_list)
+    # feature 18-23
+    feats[17] = np.mean(AoA_list)
+    feats[18] = np.mean(IMG_list)
+    feats[19] = np.mean(FAM_list)
+    feats[20] = np.std(AoA_list)
+    feats[21] = np.std(IMG_list)
+    feats[22] = np.std(FAM_list)
+    # feature 24-29
+    feats[23] = np.mean(V_list)
+    feats[24] = np.mean(A_list)
+    feats[25] = np.mean(D_list)
+    feats[26] = np.std(V_list)
+    feats[27] = np.std(A_list)
+    feats[28] = np.std(D_list)
+    return feats
+
+          
+    
+    
+def extract2(feat, comment_class, comment_id):
+    ''' This function adds features 30-173 for a single comment.
+
+    Parameters:
+        feat: np.array of length 173
+        comment_class: str in {"Alt", "Center", "Left", "Right"}
+        comment_id: int indicating the id of a comment
+
+    Returns:
+        feat : numpy Array, a 173-length vector of floating point features (this 
+        function adds feature 30-173). This should be a modified version of 
+        the parameter feats.
+    '''    
+    # print('TODO')
+    comment_class = classes.index(comment_class)
+    comment_id_index = ids[comment_class][comment_id]
+    feat[29:173] = LIWC[comment_class][comment_id_index]
+    return feat
+
+
+
+
+def main(args):
+    #Declare necessary global variables here. 
+
+    #Load data
+    data = json.load(open(args.input))
+    feats = np.zeros((len(data), 173+1))
+
+    # TODO: Call extract1 for each datatpoint to find the first 29 features. 
+    # Add these to feats.
+    for i, comment in enumerate(data):
+        comment_class = comment["cat"]
+        feat = extract1(comment["body"])
+        feats[i][:29] = feat[:29]
+    # TODO: Call extract2 for each feature vector to copy LIWC features (features 30-173)
+    # into feats. (Note that these rely on each data point's class,
+    # which is why we can't add them in extract1).
+        feats[i][29:173] = extract2(feat, comment_class, comment["id"])
+        feats[173] = classes.index(comment_class)
+    
+    # print('TODO')
+
+    np.savez_compressed(args.output, feats)
+
+    
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description='Process each .')
+    parser.add_argument("-o", "--output", help="Directs the output to a filename of your choice", required=True)
+    parser.add_argument("-i", "--input", help="The input JSON file, preprocessed as in Task 1", required=True)
+    parser.add_argument("-p", "--a1_dir", help="Path to csc401 A1 directory. By default it is set to the cdf directory for the assignment.", default="/u/cs401/A1/")
+    args = parser.parse_args()        
+
+    main(args)
+
