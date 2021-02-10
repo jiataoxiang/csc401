d6033be163a9027b93decaba5c1dd1add0d05be5
xiangji5 <jiatao.xiang@mail.utoronto.ca>
2021-02-10 14:15:06 -0500

    update

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@ mkdir -p output
 
 rm ./output/compare*
 
+python3 a1_preproc.py 1004236613 -o test.json --a1_dir ./
+
 grep "body" test.json >> ./output/compare1
 grep "body" ./output/sample_out.json >> ./output/compare2
 
